ee6f585318942148fd265725bdc62993035a52ff
ManjeetOSKY <manjeet.dahiya@osky.com.au>
2024-05-24 17:49:45 +1000

    Login

--- functions/api/register_login.js
+++ functions/api/register_login.js
@@ -1,6 +1,6 @@
 export async function onRequestPost(context) {
   const formdata = context.request.formData();
-  const username = formdata.get('user-name-');
+  const username = formdata.get('user-name-1');
   const email = formdata.get('email-1');
   // return new Response('Hello : ' + "${username} - ${email}");
   return new Response('${username} - ${email}');
